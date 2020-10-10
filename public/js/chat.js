2a3003d79a4490a1451ac50155aabdb640b3531c
ChaoWei <harry2300042@gmail.com>
2020-10-11 00:00:48 +0800

    Lec159
    Event Acknowledgements

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,7 +10,13 @@ document.querySelector('#message-form').addEventListener('submit', (e)=>{
 
     const message = e.target.elements.message.value
 
-    socket.emit('sendMessage', message)
+    socket.emit('sendMessage', message, (error)=>{
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
 })
 
 document.querySelector('#send-location').addEventListener('click',()=>{
@@ -22,8 +28,8 @@ document.querySelector('#send-location').addEventListener('click',()=>{
         socket.emit('sendLocation',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
+        },()=>{
+            console.log('Location shared!')
         })
-
     })
-    
 })

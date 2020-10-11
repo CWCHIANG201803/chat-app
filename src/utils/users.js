f651d06750c6990cf14836103363beb1f41d8805
ChaoWei <harry2300042@gmail.com>
2020-10-11 16:35:59 +0800

    Lec169
    Storing Users; PartII

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,20 +1,17 @@
 const users = []
 
-// addUser, removeUser, getUser, getUsersInRoom
 
 const addUser = ({id, username, room}) => {
-    // Clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    // Validate the data
     if (!username || !room) {
         return {
             error : 'Username and room are required!'
         }
     }
 
-    // Check for existing user
+
     const existingUser = users.find((user)=>{
         return user.room === room && user.username === username
     })
@@ -40,15 +37,20 @@ const removeUser = (id) => {
     }
 }
 
+const getUser = (id) => {
 
-addUser({
-    id: 22,
-    username: 'Harry',
-    room: 'South Philly'
-})
-console.log(users)
+    return users.find( user => user.id === id)
+}
 
-const removedUser = removeUser(22)
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user)=> user.room === room)
+}
 
-console.log(removedUser)
-console.log(users)
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
+

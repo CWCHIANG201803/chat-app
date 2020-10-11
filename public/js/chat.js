c3a25677cb45c49f26f2681e2c60be0f0f3e9f8b
ChaoWei <harry2300042@gmail.com>
2020-10-11 17:45:51 +0800

    Lec172
    Rendering User List

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,9 +7,12 @@ const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
+
+
 // Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 // Options
 const { username, room } = Qs.parse(location.search, {ignoreQueryPrefix: true})
@@ -23,6 +26,15 @@ socket.on('LocationMessage', (message)=>{
     })
     $messages.insertAdjacentHTML('beforeend', html)
 })
+
+socket.on('roomData', ({room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
 
 socket.on('message', (message)=>{
     console.log(message)
